0c78c1b84327e7d53250435ff986923e620d839f
Tarunjit Biswas <tarunjitbiswas24@gmail.com>
2026-02-04 21:20:22 +0530

    Refactor index.tsx for improved readability

--- index.tsx
+++ index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App'; // This points to your App.tsx
+import './index.css';   // This will hold your Tailwind styles
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error("Root element not found. Make sure index.html has <div id='root'></div>");
 }
 
-const root = createRoot(rootElement);
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
